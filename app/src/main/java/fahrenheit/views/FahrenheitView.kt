7c5b5ac0e89e2d1923e708bce6e7b1fb353fc52d
AlexCM <pokealex2.carrasco@gmail.com>
2025-03-31 18:59:33 -0600

    Se completo funcionalidad de conversion de grados celsius a fahrenheit

---
+++ app/src/main/java/fahrenheit/views/FahrenheitView.kt
@@ -0,0 +1,56 @@
+package fahrenheit.views
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.mobile1project.R
+import fahrenheit.viewmodels.FahrenheitViewModel
+
+@Composable
+fun FahrenheitView(fahrenheitViewModel: FahrenheitViewModel = viewModel()) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.termometro),
+            contentDescription = "Termometro",
+            modifier = Modifier
+                .size(150.dp)
+                .clip(CircleShape)
+        )
+
+        TextField(
+            value = fahrenheitViewModel.celsius,
+            onValueChange = { fahrenheitViewModel.celsius = it },
+            label = { Text(stringResource(R.string.celsius)) },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick = { fahrenheitViewModel.calcularFahrenheit() },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(stringResource(R.string.conversion))
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(stringResource(R.string.fahrenheit))
+        Text("${fahrenheitViewModel.resultado}", style = MaterialTheme.typography.headlineMedium)
+    }
+}
